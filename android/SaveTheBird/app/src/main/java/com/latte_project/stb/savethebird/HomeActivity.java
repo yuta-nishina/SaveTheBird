c546ad9abdd5e18232009d258e8e3e74d44a0f07
Ari <nyarisuke@gmail.com>
2016-05-16 17:42:38 +0900

    画面構成追加

---
+++ android/SaveTheBird/app/src/main/java/com/latte_project/stb/savethebird/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.latte_project.stb.savethebird;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.View;
+import android.content.Intent;
+
+public class HomeActivity extends TabActivity {
+
+    @Override
+    // 初期表示
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // タイトル消し
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_home);
+    }
+
+    // ボタン操作
+    public void onClick(View view){
+        switch (view.getId()){
+
+            case R.id.playBtn:
+                Intent intentGame = new Intent(this, GameActivity.class);
+                startActivity(intentGame);
+                break;
+        }
+    }
+}
---
+++ android/SaveTheBird/app/src/main/java/com/latte_project/stb/savethebird/SettingActivity.java
@@ -0,0 +1,12 @@
+package com.latte_project.stb.savethebird;
+
+import android.os.Bundle;
+
+public class SettingActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
