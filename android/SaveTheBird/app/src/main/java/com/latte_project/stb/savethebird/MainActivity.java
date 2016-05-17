9ae3b910f1d4d3aa9a7b666b4124d88572d02f66
Ari <nyarisuke@gmail.com>
2016-05-17 10:38:47 +0900

    BGM追加

--- android/SaveTheBird/app/src/main/java/com/latte_project/stb/savethebird/MainActivity.java
+++ android/SaveTheBird/app/src/main/java/com/latte_project/stb/savethebird/MainActivity.java
@@ -1,14 +1,15 @@
 package com.latte_project.stb.savethebird;
 
 import android.app.Activity;
-//import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+import android.media.MediaPlayer;
 
-//public class MainActivity extends AppCompatActivity {
 public class MainActivity extends Activity {
+
+    private MediaPlayer player;
 
     @Override
     // 初期表示
@@ -19,6 +20,23 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_main);
+
+        // 再生中なら停止
+        if (player != null && player.isPlaying()) {
+            player.stop(); // 停止
+            player.release();// メモリの解放
+        }
+        // 再生
+        try {
+            player = MediaPlayer.create(this, R.raw.sample);
+            player.setLooping(true);  // 連続再生設定
+            player.start(); // 再生
+
+        } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+
 
         Handler handler = new Handler();
         handler.postDelayed(new splashHandler(), 2000);
